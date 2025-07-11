252a8bb56e6659c859548c76efc9eeb6ab20fd7d
+ <+githinjiadreen27@gmail.com>
2025-07-11 12:16:46 +0300

    Using State Hooks and Events

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  //State to track selected category
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  //Filter items based on selected category
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
+
+  //Update state on the select change
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" value={selectedCategory} onChange={handleCategoryChange}>
+          <option value="All">Filter All</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
